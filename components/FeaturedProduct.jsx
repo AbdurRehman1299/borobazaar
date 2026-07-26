102233841d76c41799e3b6525503ab0a2a5984dc
Abdur Rehman <abdurrehman76001@gmail.com>
2026-07-26 15:30:16 +0500

    modify featured product component

--- components/FeaturedProduct.jsx
+++ components/FeaturedProduct.jsx
@@ -24,7 +24,7 @@ const products = [
 
 const FeaturedProduct = () => {
   return (
-    <div className="mt-14">
+    <section className="mt-14">
       <div className="flex flex-col items-center">
         <p className="text-3xl font-medium">Featured Products</p>
         <div className="w-28 h-0.5 bg-orange-600 mt-2"></div>
@@ -55,7 +55,7 @@ const FeaturedProduct = () => {
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
