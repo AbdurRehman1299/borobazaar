995f6a46978a4cc59f7b8f11e897045e86160b61
Abdur Rehman <abdurrehman76001@gmail.com>
2026-07-26 19:07:11 +0500

    create all products page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
         <div className="w-4/5">
           <Image className="w-28 md:w-32 mb-5" src={assets.logo} alt="logo" />
-          <p className="mt-b text-sm ml-0.5">
+          <p className="mt-b text-sm">
             Discover a curated collection of products designed to elevate your
             everyday life. From the latest trends to timeless classics, we bring
             you quality, style, and value all in one place. Enjoy secure
