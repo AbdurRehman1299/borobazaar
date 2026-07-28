7946d66c5daae1e7b602d0be33e0eace48fb2d66
Abdur Rehman <abdurrehman76001@gmail.com>
2026-07-28 21:07:02 +0500

    update seller footer component

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -1,31 +1,31 @@
-import { assets } from "@/lib/assets"
-import Image from "next/image"
-import Link from "next/link"
+import { assets } from "@/lib/assets";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <section className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-10">
-        <div className="flex items-center gap-4">
-            <Image className="hidden md:block" src={assets.logo} alt="logo" />
-            <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
-            <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-                Copyright 2026 &copy; AbdurRehman. All Rights Reserved.
-            </p>
-        </div>
+      <div className="flex items-center gap-4">
+        <Image className="hidden md:block" src={assets.logo} alt="logo" />
+        <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
+        <p className="py-4 text-center text-xs md:text-sm text-gray-500">
+          Copyright 2026 &copy; AbdurRehman. All Rights Reserved.
+        </p>
+      </div>
 
-        <div className="flex items-center gap-3">
-            <Link href={"#"}>
-                <Image src={assets.facebook_icon} alt="facebook-icon" />
-            </Link>
-            <Link href={"#"}>
-                <Image src={assets.twitter_icon} alt="twitter-icon" />
-            </Link>
-            <Link href={"#"}>
-                <Image src={assets.instagram_icon} alt="instagram-icon" />
-            </Link>
-        </div>
+      <div className="flex items-center gap-3">
+        <Link href={"#"}>
+          <Image src={assets.facebook_icon} alt="facebook-icon" />
+        </Link>
+        <Link href={"#"}>
+          <Image src={assets.twitter_icon} alt="twitter-icon" />
+        </Link>
+        <Link href={"#"}>
+          <Image src={assets.instagram_icon} alt="instagram-icon" />
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
