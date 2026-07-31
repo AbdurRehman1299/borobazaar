c82d498a60e7eb4921205f24221c3cf915197257
Abdur Rehman <abdurrehman76001@gmail.com>
2026-07-31 18:27:08 +0500

    feature: add sorting and lean method to display orders

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -1,5 +1,7 @@
 import connectDB from "@/config/db";
+import Address from "@/models/Address";
 import Order from "@/models/Order";
+import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -11,7 +13,10 @@ export async function GET(request) {
 
     const orders = await Order.find({ userId })
       .sort({ date: -1 })
-      .populate("address items.product")
+      .populate([
+        { path: "address", model: Address },
+        { path: "items.product", model: Product },
+      ])
       .lean();
 
     return NextResponse.json(
